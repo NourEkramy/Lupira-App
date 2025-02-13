aabedab14db6d8daa682eb2ce4647448c718c2c5
NourEkramy <nourekramy66@gmail.com>
2025-02-13 19:58:55 +0200

    we made the card module

---
+++ lib/Modules/user_credentials_card_module.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class UserCredentialsCardModule extends StatelessWidget {
+
+  Widget cardBody;
+
+  UserCredentialsCardModule({super.key, required this.cardBody});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(gradient: LinearGradient(begin: Alignment.topLeft,end: Alignment.bottomRight,colors: [Color(0xFF744199),Color(0xFFEBE4E4)],
+      ),
+    ),
+
+    child: Container(
+    decoration: BoxDecoration(color: Color(0xFFEBE4E4),border:Border.all(color: Colors.transparent,width: 1,), borderRadius: BorderRadius.circular(10),),
+    margin: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width*0.07,vertical: MediaQuery.sizeOf(context).height*0.075),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: cardBody,
+        ),
+      ),
+    );
+  }
+}
