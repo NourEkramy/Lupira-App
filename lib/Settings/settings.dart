ea15f39553efaf700b8748dade0147216d42293b
NourEkramy <nourekramy66@gmail.com>
2025-02-14 20:38:22 +0200

    we made the first row in the settings then we will make its module now

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -9,125 +9,50 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Color(0xFFDEDAE0),
       body: Column(
         children: [
           AppBar(
             backgroundColor: Color(0xFF744199),
             title: Text(
               'Settigns',
-              style: TextStyle(fontSize:40,fontFamily: 'Alegreya',color: Color(0xFFEBE4E4),),
+              style: TextStyle(
+                fontSize: 40,
+                fontFamily: 'Alegreya',
+                color: Color(0xFFEBE4E4),
+              ),
             ),
           ),
           Expanded(
-          child:SingleChildScrollView(
-            child:  Column(
-            children: [
-          Column(
-            children:[
-              Row(children: [
-                Image.asset('')
-              ],),
-              Divider(),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your sign-up logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFDEDAE0),
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Color(0xFFDEDAE0), width: 2,),
-                    borderRadius: BorderRadius.circular(0),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(children: [
+                  GestureDetector(
+                    onTap: (){},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 35,vertical: 13),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/images/man 2.png'),
+                          SizedBox(width: MediaQuery.sizeOf(context).width * 0.065),
+                          Text(
+                            'Profile',
+                            style: TextStyle(
+                              color: Color(0xFF4B4A4C),
+                              fontFamily: 'Inder',
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  minimumSize: Size(double.infinity, 50),
-                ),
-                child: Text(
-                  'Change password',
-                  style: TextStyle(
-                    color: Color(0xFF4B4A4C),
-                    fontFamily: 'Inder',
-                    fontSize: 24,
-                  ),
-                ),
+                  Divider(thickness: 1,color: Color(0xFFABABAB),),
+                ]),
               ),
-              Divider(),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your sign-up logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFDEDAE0),
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Color(0xFFDEDAE0), width: 2,),
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  minimumSize: Size(double.infinity, 50),
-                ),
-                child: Text(
-                  'Language',
-                  style: TextStyle(
-                    color: Color(0xFF4B4A4C),
-                    fontFamily: 'Inder',
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              Divider(),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your sign-up logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFDEDAE0),
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Color(0xFFDEDAE0), width: 2,),
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  minimumSize: Size(double.infinity, 50),
-                ),
-                child: Text(
-                  'Delete account',
-                  style: TextStyle(
-                    color: Color(0xFF4B4A4C),
-                    fontFamily: 'Inder',
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              Divider(),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your sign-up logic here
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFDEDAE0),
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Color(0xFFDEDAE0), width: 2,),
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  minimumSize: Size(double.infinity, 50),
-                ),
-                child: Text(
-                  'Log out',
-                  style: TextStyle(
-                    color: Color(0xFFD6101D),
-                    fontFamily: 'Inder',
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              Divider(),
-            ]
-
-          ),
-                    ]
             ),
-                    ),
-                    ),
+          ),
         ],
       ),
     );
