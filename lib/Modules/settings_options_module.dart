4c3e0ae447a6825f0b70e2fb7c5dc8f23b54eb61
NourEkramy <nourekramy66@gmail.com>
2025-02-14 20:47:08 +0200

    we made the settings option module and we will apply it now on the settings page

---
+++ lib/Modules/settings_options_module.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SettingsOptionsModule extends StatelessWidget {
+  String optionIcon;
+  String optionText;
+  Color optionTextColor;
+  SettingsOptionsModule({super.key, required this.optionTextColor, required this.optionIcon, required this.optionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(children: [
+        GestureDetector(
+          onTap: (){},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35,vertical: 13),
+            child: Row(
+              children: [
+                Image.asset(optionIcon),
+                SizedBox(width: MediaQuery.sizeOf(context).width * 0.065),
+                Text(
+                  optionText,
+                  style: TextStyle(
+                    color: optionTextColor,
+                    fontFamily: 'Inder',
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(thickness: 1,color: Color(0xFFABABAB),),
+      ]),
+    );
+  }
+}
