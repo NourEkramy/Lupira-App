31b3e0c0d8c22c05b9394e7e0f1c6db4f71eaedb
NourEkramy <nourekramy66@gmail.com>
2025-02-14 20:16:32 +0200

    edited the devleopment after merging with the fogot password

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/Password/forgot_password.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: ForgotPassword.routName,
       routes: {
-        ForgotPassword.routName: (context) => ForgotPassword(),
       },
     );
   }
