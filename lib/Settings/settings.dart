4e5dba36dc089e16993f54b736a584d9b98090a4
NourEkramy <nourekramy66@gmail.com>
2025-02-14 20:11:15 +0200

    .

--- lib/Settings/settings.dart
+++ lib/Settings/settings.dart
@@ -8,43 +8,26 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          backgroundColor: Color(0xFF744199),
-          title: Text(
-            'Settigns',
-            style: TextStyle(fontSize:40,fontFamily: 'Alegreya',color: Color(0xFFEBE4E4),),
+    return Scaffold(
+
+      body: Column(
+        children: [
+          AppBar(
+            backgroundColor: Color(0xFF744199),
+            title: Text(
+              'Settigns',
+              style: TextStyle(fontSize:40,fontFamily: 'Alegreya',color: Color(0xFFEBE4E4),),
+            ),
           ),
-        ),
-        Container(color: Color(0xFFDEDAE0),
-          child: Expanded(
+          Expanded(
           child:SingleChildScrollView(
             child:  Column(
             children: [
           Column(
-            children:[ElevatedButton(
-              onPressed: () {
-                // Add your sign-up logic here
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFDEDAE0),
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Color(0xFFDEDAE0), width: 2,),
-                  borderRadius: BorderRadius.circular(0),
-                ),
-                minimumSize: Size(double.infinity, 50),
-              ),
-              child: Text(
-                'Profile',
-                style: TextStyle(
-                  color: Color(0xFF4B4A4C),
-                  fontFamily: 'Inder',
-                  fontSize: 24,
-                ),
-              ),
-            ),
+            children:[
+              Row(children: [
+                Image.asset('')
+              ],),
               Divider(),
               ElevatedButton(
                 onPressed: () {
@@ -141,11 +124,12 @@ class Settings extends StatelessWidget {
             ]
 
           ),
-        ]
+                    ]
             ),
-        ),
-        ),),
-      ],
+                    ),
+                    ),
+        ],
+      ),
     );
   }
 }
