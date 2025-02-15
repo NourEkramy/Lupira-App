ed6e35a457fc82edda51f8f64e013723eae32296
NourEkramy <nourekramy66@gmail.com>
2025-02-15 18:47:53 +0200

    made the about lupira but needs to add the assets to the dev bransh then merge

---
+++ lib/About-Lupira/about_lupira.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AboutLupira extends StatelessWidget {
+  AboutLupira({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
