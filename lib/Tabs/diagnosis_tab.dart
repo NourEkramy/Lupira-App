955bb914bb72e0ba950e5ce8cffd631db77e3bd3
Rojena <rojenamohamaden@gmail.com>
2025-02-18 01:42:30 +0200

    Add report card module to the ui with redunduncy

--- lib/Tabs/diagnosis_tab.dart
+++ lib/Tabs/diagnosis_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/Modules/detection_card_module.dart';
+import 'package:untitled/Modules/report_card_module.dart';
 
 class DiagnosisTab extends StatelessWidget {
   const DiagnosisTab({super.key});
@@ -74,51 +75,25 @@ class DiagnosisTab extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: const Color(0xffD2CFD3),
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2), // Shadow color with opacity
-                    spreadRadius: 0, // Spread of the shadow
-                    blurRadius: 8, // Softness of the shadow
-                    offset: const Offset(0, 4), // Position of the shadow (X, Y)
-                  ),
-                ],
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 15,),
-              margin: const EdgeInsets.only(top: 15,),
-              width: MediaQuery.of(context).size.width * 0.94,
-              child: Row(
-                children: [
-                  Image.asset("assets/images/medical-clipboard .png"),
-                  SizedBox(width: MediaQuery.of(context).size.width*0.05,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "5 May, 2024",
-                        style: TextStyle(
-                          color: Color(0xFF4B4A4C),
-                          fontFamily: "Inder",
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        "No signs of lupus detected",
-                        style: TextStyle(
-                          color: Color(0xFF817F82),
-                          fontFamily: "Inder",
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            ReportCardModule(
+              reportDate: "5 May, 2024",
+              reportResult: "No signs of lupus detected",
+            ),
+            ReportCardModule(
+              reportDate: "5 May, 2024",
+              reportResult: "No signs of lupus detected",
+            ),
+            ReportCardModule(
+              reportDate: "5 May, 2024",
+              reportResult: "No signs of lupus detected",
+            ),
+            ReportCardModule(
+              reportDate: "5 May, 2024",
+              reportResult: "No signs of lupus detected",
+            ),
+            ReportCardModule(
+              reportDate: "5 May, 2024",
+              reportResult: "No signs of lupus detected",
             ),
           ],
         ),
