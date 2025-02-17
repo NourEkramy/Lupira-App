e82c55f93c323965d35c54a19c92c541f580b018
Rojena <rojenamohamaden@gmail.com>
2025-02-18 00:26:34 +0200

    Add Detection Card Module to the UI

--- lib/Tabs/diagnosis_tab.dart
+++ lib/Tabs/diagnosis_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Modules/detection_card_module.dart';
 
 class DiagnosisTab extends StatelessWidget {
   const DiagnosisTab({super.key});
@@ -28,77 +29,7 @@ class DiagnosisTab extends StatelessWidget {
                 ),
               ),
             ),
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: MediaQuery.of(context).size.height * 0.224,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFCABDD3),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(20.0),
-                  margin: const EdgeInsets.only(
-                    top: 6,
-                    right: 4,
-                    bottom: 50,
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "Start Lupus Detection",
-                              style: TextStyle(
-                                color: Color(0xFF3D1A57),
-                                fontFamily: "Inder",
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xFFCABDD3),
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 25,
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  side: const BorderSide(
-                                    color: Color(0xFF502371),
-                                    width: 2,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                              ),
-                              child: const Text(
-                                "Start Now",
-                                style: TextStyle(
-                                  color: Color(0xFF502371),
-                                  fontFamily: 'Inder',
-                                  fontSize: 17,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: -10,
-                  right: -15,
-                  child: Image.asset("assets/images/detection image.png"),
-                ),
-              ],
-            ),
+            DetectionCardModule(mainTitle: "Start Lupus Detection"),
              Padding(
               padding: const EdgeInsets.only(
                 left: 10,
