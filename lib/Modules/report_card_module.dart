166585ead8f0536d8dc2d8f273f29a87d9a2feef
Rojena <rojenamohamaden@gmail.com>
2025-02-18 18:57:07 +0200

    Modify the margin of report card module

--- lib/Modules/report_card_module.dart
+++ lib/Modules/report_card_module.dart
@@ -30,7 +30,7 @@ class ReportCardModule extends StatelessWidget {
         horizontal: 15,
       ),
       margin: const EdgeInsets.only(
-        top: 20,
+        bottom: 25,
       ),
       width: MediaQuery.of(context).size.width * 0.94,
       child: Row(
