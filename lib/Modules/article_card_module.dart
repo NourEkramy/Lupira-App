e6cfdbc20cd270ddeb48e2d522770f0d7976c9b0
NourEkramy <nourekramy66@gmail.com>
2025-02-25 20:31:48 +0200

    edites on the article module

--- lib/Modules/article_card_module.dart
+++ lib/Modules/article_card_module.dart
@@ -58,7 +58,7 @@ class ArticleCardModule extends StatelessWidget {
               articleName,
               style: TextStyle(
                 fontSize: 20,
-                fontWeight: FontWeight.bold,
+                fontWeight: FontWeight.w500,
                 fontFamily: 'Inder',
                 color: Color(0xFF4B4A4C),
               ),
