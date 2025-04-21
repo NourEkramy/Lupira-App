0930b68a7a5e6b5c829e06f354eff383a1ea61b0
NourEkramy <nourekramy66@gmail.com>
2025-04-21 16:48:11 +0200

    we finished the forgot password validation

--- lib/Password/forgot_password.dart
+++ lib/Password/forgot_password.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:untitled/Modules/operation_button_module.dart';
 import 'package:untitled/Modules/text_field_module.dart';
 import 'package:untitled/Modules/user_credentials_card_module.dart';
 import 'forgot_password_api.dart';
-
-bool emailSentFlag = false;
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({Key? key}) : super(key: key);
@@ -16,75 +16,138 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
-  TextEditingController emailController = TextEditingController();
+  final _formKey = GlobalKey<FormBuilderState>();
+  var codeState;
+  late String message;
 
   void forgotPassword() async {
-    String email = emailController.text;
-    String userToken =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwMjAsImlhdCI6MTc0NDE5MjQ2MSwiZXhwIjoxNzQ0Nzk3MjYxfQ.ggIwy6pFWSEkowZs9dnKGWuYez08qKC-uFOJ9QB5hHQ";
-    bool success = await ForgotPasswordApi.forgotPassword(
-      email: email,
-      token: userToken,
-    );
+    if (_formKey.currentState?.saveAndValidate() ?? false) {
+      final values = _formKey.currentState!.value;
 
-    if (success) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Email sent successful")),
-      );
+      String email = values['Email'];
 
-      setState(() {
-        emailSentFlag = true;
-      });
+      try {
+        final response = await ForgotPasswordApi.forgotPassword(
+          email: email,
+        );
 
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Failed to send email")),
-      );
-      setState(() {
-        emailSentFlag = true;
-      });
+        bool success = response['success'];
+        message = response['message'];
+        var code = response['code'];
+
+        if (code == 200)
+          setState(() {
+            codeState = 200;
+          });
+        else if (code == 404) {
+          setState(() {
+            codeState = 404;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                message,
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Color(0xFFB9433E),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              behavior: SnackBarBehavior.floating,
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              duration: Duration(seconds: 3),
+            ),
+          );
+        } else
+          setState(() {
+            codeState = 500;
+          });
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("$e"),
+            backgroundColor: Color(0xFFB9433E),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            behavior: SnackBarBehavior.floating,
+            margin: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+          ),
+        );
+      }
     }
+    // if (success) {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(content: Text("Email sent successful")),
+    //   );
+    //
+    //   setState(() {
+    //     emailSentFlag = true;
+    //   });
+    // } else {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(content: Text("Failed to send email")),
+    //   );
+    //   setState(() {
+    //     emailSentFlag = true;
+    //   });
+    // }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: UserCredentialsCardModule(
-        cardBody: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.06),
-            Text(emailSentFlag ? 'Verification email': 'Forgot password?',
-              style: TextStyle(
-                fontSize: 43,
-                fontFamily: 'Alegreya',
-                color: Color(0xFF49146D),
+        cardBody: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.06),
+              Text(
+                (codeState == 200 || codeState == 500)
+                    ? 'Verification email'
+                    : 'Forgot password?',
+                style: TextStyle(
+                  fontSize: 43,
+                  fontFamily: 'Alegreya',
+                  color: Color(0xFF49146D),
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.003),
-            Text(
-              emailSentFlag ? 'We’ve sent a verification link to your email': 'Enter your Email account to reset password',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                fontFamily: 'Inder',
-                color: Color(0xFF817F82),
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.003),
+              Text(
+                (codeState == 200 || codeState == 500)
+                    ? message
+                    : 'Enter your Email account to reset password',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Inder',
+                  color: codeState == 500 ? Color(0xFFB9433E) : Color(0xFF817F82),
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.sizeOf(context).height * 0.003),
-            Image.asset(
-              'assets/images/image.png', // Replace with your image path
-              height: MediaQuery.sizeOf(context).height * 0.27,
-              width: MediaQuery.sizeOf(context).width * 0.58,
-              fit: BoxFit.fill, // Adjust the fit as needed
-            ),
-            Expanded(
-              child: SingleChildScrollView(
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.003),
+              Image.asset(
+                'assets/images/image.png', // Replace with your image path
+                height: MediaQuery.sizeOf(context).height * 0.27,
+                width: MediaQuery.sizeOf(context).width * 0.58,
+                fit: BoxFit.fill, // Adjust the fit as needed
+              ),
+              FormBuilder(
+                key: _formKey,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TextFieldModule(
-                      textController: emailController,
+                      name: 'Email',
+                      validators: [
+                        FormBuilderValidators.required(
+                            errorText: 'Email is required'),
+                        FormBuilderValidators.email(
+                            errorText: 'Please enter a valid email address!'),
+                      ],
                       textFieldType: TextInputType.emailAddress,
                       hintTextTitle: 'Enter Email',
                       textFieldTitle: 'Email',
@@ -93,15 +156,19 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                       borderColor: Color(0xFFDEDAE0),
                       backgroundColor: Color(0xFFDEDAE0),
                     ),
-                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.045),
+                    SizedBox(
+                        height: MediaQuery.sizeOf(context).height * 0.045),
                     OperationButtonModule(
                       borderColor: Color(0xFF502371),
                       buttonColor: Color(0xFF502371),
-                      buttonText: emailSentFlag ? 'Resend email': 'Send verification email',
+                      buttonText: (codeState == 200 || codeState == 500)
+                          ? 'Resend email'
+                          : 'Send verification email',
                       buttonTextColor: Colors.white,
                       onTap: forgotPassword,
                     ),
-                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.025),
+                    SizedBox(
+                        height: MediaQuery.sizeOf(context).height * 0.025),
                     OperationButtonModule(
                       borderColor: Color(0xFF502371),
                       buttonColor: Color(0xFFEBE4E4),
@@ -109,11 +176,13 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                       buttonTextColor: Color(0xFF502371),
                       onTap: () {},
                     ),
+                    SizedBox(
+                        height: MediaQuery.sizeOf(context).height * 0.03),
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/Password/forgot_password_api.dart
+++ lib/Password/forgot_password_api.dart
@@ -1,16 +1,15 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class ForgotPasswordApi{
-  static Future<bool> forgotPassword({
+class ForgotPasswordApi {
+  static Future<Map<String, dynamic>> forgotPassword({
     required String email,
-    required String token,
+
   }) async {
     Uri url = Uri.parse("https://lupira.onrender.com/api/auth/password/forgot");
 
     final headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer $token'
     };
 
     final body = jsonEncode({
@@ -23,10 +22,33 @@ class ForgotPasswordApi{
       body: body,
     );
 
+    final decoded = jsonDecode(response.body);
+
     if (response.statusCode == 200 || response.statusCode == 201) {
-      return true;
+      try {
+        return {
+          'success': true,
+          'code': 200,
+          'message': decoded['message'] ?? 'Password reset email sent successfully. Please check your inbox.',
+        };
+      } catch (e) {
+        throw FormatException("Invalid JSON format: ${response.body}");
+      }
     } else {
-      return false;
+      if (response.statusCode == 404) {
+        return {
+          'success': false,
+          'code': 404,
+          'message': decoded['error'] ?? "User with this email does not exist.",
+        };
+      } else {
+        return {
+          'success': false,
+          'code': 500,
+          'message': decoded['error'] ??
+              "Failed to send reset email. Please try again.",
+        };
+      }
     }
   }
 }
