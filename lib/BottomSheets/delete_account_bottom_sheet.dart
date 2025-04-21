b1605adcebb0e8fa33ce86884c486b1b25e2b0c6
NourEkramy <nourekramy66@gmail.com>
2025-04-21 21:16:34 +0200

    we finished the delete account api but the token is not fixed and we need to edit return delete at the ui

--- lib/BottomSheets/delete_account_bottom_sheet.dart
+++ lib/BottomSheets/delete_account_bottom_sheet.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/BottomSheets/delete_account_api.dart';
 import 'package:untitled/Modules/operation_button_module.dart';
 
 class DeleteAccountBottomSheet extends StatelessWidget {
   const DeleteAccountBottomSheet({super.key});
+
+  Future<void> _deleteAccount(BuildContext context) async {
+    String token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI1MjAsImlhdCI6MTc0NTI2MjY3MSwiZXhwIjoxNzQ1MjY2MjcxfQ.EUeJ_kPUdwuhcZ25W8LHIttBSKfRVENcoeTwSNyQgaw";
+
+    bool success = await DeleteAccountApi.deleteAccount(token);
+
+    if (success) {
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Account deleted successfully.')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete account.')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,6 +75,9 @@ class DeleteAccountBottomSheet extends StatelessWidget {
                     buttonColor: Color(0xFF502371),
                     buttonText: 'Cancel',
                     buttonTextColor: Color(0xFFFFFFFF),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
                   ),
                   SizedBox(height: MediaQuery.sizeOf(context).height * 0.025),
                   OperationButtonModule(
@@ -63,6 +85,9 @@ class DeleteAccountBottomSheet extends StatelessWidget {
                     buttonColor: Color(0xFFEBE4E4),
                     buttonText: 'Delete account',
                     buttonTextColor: Color(0xFFD6101D),
+                    onTap: () {
+                      _deleteAccount(context);
+                    },
                   ),
                   SizedBox(height: MediaQuery.sizeOf(context).height * 0.015),
                 ],
